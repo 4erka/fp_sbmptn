8cc5c426cdb55bd7ebd39ad2910db0778ac8f589
mhanifamrizal <mhanifamrizal@gmail.com>
2017-05-22 04:12:59 +0700

    edit

---
+++ application/views/formbiodata.php
@@ -0,0 +1,62 @@
+<div class="container">
+  <div class="row">
+  <h1 style="text-align: center;">PENDAFTARAN SBMPTN 2017</h1>
+  <h3 style="text-align: center;">Biodata</h3>
+    <div class="col-md-8 col-md-offset-2">
+      <form class="form-horizontal" action="/action_page.php">
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="Nama_peserta">Nama:</label>
+          <div class="col-sm-6">
+            <input type="text" class="form-control" id="Nama_peserta" placeholder="Nama Lengkap" name="Nama_peserta">
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="alamat">alamat tetap:</label>
+          <div class="col-sm-6">          
+            <input type="text" class="form-control" id="alamat" placeholder="alamat" name="alamat">
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="tgl_lhr">tanggal lahir:</label>
+          <div class="col-sm-6">          
+            <input type="date" class="form-control" id="tgl_lhr" placeholder="tanggal lahir" name="tgl_lhr">
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="tmp_lhr">tempat lahir:</label>
+          <div class="col-sm-6">          
+            <input type="text" class="form-control" id="tmp_lhr" placeholder="tempat lahir" name="tmp_lhr">
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="nohp">No Handphone:</label>
+          <div class="col-sm-6">          
+            <input type="text" class="form-control" id="nohp" placeholder="Nomor Handphone" name="nohp">
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="email">Email:</label>
+          <div class="col-sm-6">          
+            <input type="Email" class="form-control" id="email" placeholder="Email" name="email">
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="pwd">Password:</label>
+          <div class="col-sm-6">          
+            <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="pwd">Password:</label>
+          <div class="col-sm-6">          
+            <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
+          </div>
+        </div>
+      </form>
+    </div>
+
+  
+
+      
+    </div>
+</div>
