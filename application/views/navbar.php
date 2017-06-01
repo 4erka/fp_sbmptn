231e5ee1964a2c0758d6713a103b2395df6cf223
Kukuh Rilo Pambudi <rilokukuh@gmail.com>
2017-06-01 22:48:40 +0700

    daftar panlok

--- application/views/navbar.php
+++ application/views/navbar.php
@@ -13,7 +13,7 @@
           <li><a href="<?php echo site_url('con_pageinformasi') ?>">Informasi Umum</a></li>
           <li><a href="<?php echo site_url('Daftarptn') ?>">Daftar PTN</a></li>
           <li><a href="<?php echo site_url('con_daftarptnuk') ?>">Daftar PTN UK</a></li>
-          <li><a href="<?php echo site_url('con_daftarpanlok') ?>">Daftar Panlok</a></li>
+          <li><a href="<?php echo site_url('DaftarPanlok') ?>">Daftar Panlok</a></li>
         </ul>
       </li>
       
