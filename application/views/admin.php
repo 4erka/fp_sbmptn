b011cd79202c0e033e17a6f6bb8f3f24bded2928
mhanifamrizal <mhanifamrizal@gmail.com>
2017-06-01 13:34:56 +0700

    qwertyu

---
+++ application/views/admin.php
@@ -0,0 +1,20 @@
+<div class="row">
+                    <div class="col-lg-8">
+                        <h2>Nama Peserta</h2>
+                        <div class="table-responsive">
+                            <table class="table table-bordered table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Nama</th>
+                                        <th>% New Visits</th>
+                                        <th>Revenue</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    </div>
