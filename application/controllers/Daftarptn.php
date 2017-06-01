7a0ed93fa1eb9dfa1a23c50853600152db89bd8c
Kukuh Rilo Pambudi <rilokukuh@gmail.com>
2017-06-01 22:23:31 +0700

    daftar ptn

--- application/controllers/Daftarptn.php
+++ application/controllers/Daftarptn.php
@@ -7,12 +7,16 @@ class Daftarptn extends CI_Controller {
 	{
 		parent::__construct();
 		$this->load->helper('url');
+        $this->load->helper('form');
+        $this->load->database();
 	}
 
 	public function index()
 	{
-		$daftarptn = $this->db->query('SELECT* FROM ptn');
-		$resDaftarptn = array
+		$daftarptn = $this->db->query('SELECT ptn_id, ptn_nama, ptn.wil_id, ptn.pl_id, pl_nama
+			FROM ptn
+			INNER JOIN panlok ON ptn.pl_id = panlok.pl_id');
+		$resDaftarptn = array(
                 'daftarptn' => $daftarptn->result()
             );
 		$this->load->view('header');
--- application/views/daftarptn.php
+++ application/views/daftarptn.php
@@ -2,8 +2,8 @@
   <li class="active"><a data-toggle="tab" href="#home">Wilayah 1</a></li>
   <li><a data-toggle="tab" href="#menu1">Wilayah 2</a></li>
   <li><a data-toggle="tab" href="#menu2">Wilayah 3</a></li>
-  <li><a data-toggle="tab" href="#menu2">Wilayah 4</a></li>
-  <li><a data-toggle="tab" href="#menu2">Semua Wilayah</a></li>
+  <li><a data-toggle="tab" href="#menu3">Wilayah 4</a></li>
+  <li><a data-toggle="tab" href="#menu4">Semua Wilayah</a></li>
 </ul>
 
 <div class="tab-content">
@@ -21,17 +21,20 @@
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>10</td>
-            <td>ISBI ACEH (http://www.isbiaceh.ac.id/)</td>
-            <td><button type="button" class="btn btn-info">Lihat Profil</button></td>
-            <td>1</td>
-            <td>11 - BANDA ACEH</td>
-          </tr>
+          <?php foreach($daftarptn as $res): ?>
+            <?php if($res->wil_id == 1): ?>
+            <tr>   
+                <td><?php echo $res->ptn_id; ?></td>
+                <td><?php echo $res->ptn_nama; ?></td>
+                <td><button type="button" class="btn btn-info">Lihat Profil</button></td>
+                <td><?php echo $res->wil_id; ?></td>
+                <td><?php echo $res->pl_nama; ?></td>
+            </tr>
+          <?php endif;?>
+          <?php endforeach; ?>
         </tbody>
       </table>
     </div>
-
   </div>
   <div id="menu1" class="tab-pane fade">
     <div class="container">
@@ -46,20 +49,105 @@
             <th>PANLOK</th>
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            <td>10</td>
-            <td>ISBI ACEH (http://www.isbiaceh.ac.id/)</td>
-            <td><button type="button" class="btn btn-info">Lihat Profil</button></td>
-            <td>1</td>
-            <td>11 - BANDA ACEH</td>
-          </tr>
+          <tbody>
+            <?php foreach($daftarptn as $res): ?>
+            <?php if($res->wil_id == 2): ?>
+            <tr>   
+                <td><?php echo $res->ptn_id; ?></td>
+                <td><?php echo $res->ptn_nama; ?></td>
+                <td><button type="button" class="btn btn-info">Lihat Profil</button></td>
+                <td><?php echo $res->wil_id; ?></td>
+                <td><?php echo $res->pl_nama; ?></td>
+            </tr>
+          <?php endif;?>
+          <?php endforeach; ?>
         </tbody>
       </table>
     </div>
   </div>
   <div id="menu2" class="tab-pane fade">
-    <h3>Menu 2</h3>
-    <p>Some content in menu 2.</p>
+    <div class="container">
+      <h2>DAFTAR PTN DAN PANLOK</h2>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>KODE</th>
+            <th>NAMA</th>
+            <th>PRODI</th>
+            <th>WILAYAH</th>
+            <th>PANLOK</th>
+          </tr>
+        </thead>
+          <tbody>
+            <?php foreach($daftarptn as $res): ?>
+            <?php if($res->wil_id == 3): ?>
+            <tr>   
+                <td><?php echo $res->ptn_id; ?></td>
+                <td><?php echo $res->ptn_nama; ?></td>
+                <td><button type="button" class="btn btn-info">Lihat Profil</button></td>
+                <td><?php echo $res->wil_id; ?></td>
+                <td><?php echo $res->pl_nama; ?></td>
+            </tr>
+          <?php endif;?>
+          <?php endforeach; ?>
+        </tbody>
+      </table>
+    </div>
+  </div>
+  <div id="menu3" class="tab-pane fade">
+    <div class="container">
+      <h2>DAFTAR PTN DAN PANLOK</h2>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>KODE</th>
+            <th>NAMA</th>
+            <th>PRODI</th>
+            <th>WILAYAH</th>
+            <th>PANLOK</th>
+          </tr>
+        </thead>
+          <tbody>
+            <?php foreach($daftarptn as $res): ?>
+            <?php if($res->wil_id == 4): ?>
+            <tr>   
+                <td><?php echo $res->ptn_id; ?></td>
+                <td><?php echo $res->ptn_nama; ?></td>
+                <td><button type="button" class="btn btn-info">Lihat Profil</button></td>
+                <td><?php echo $res->wil_id; ?></td>
+                <td><?php echo $res->pl_nama; ?></td>
+            </tr>
+          <?php endif;?>
+          <?php endforeach; ?>
+        </tbody>
+      </table>
+    </div>
+  </div>
+  <div id="menu4" class="tab-pane fade">
+    <div class="container">
+      <h2>DAFTAR PTN DAN PANLOK</h2>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>KODE</th>
+            <th>NAMA</th>
+            <th>PRODI</th>
+            <th>WILAYAH</th>
+            <th>PANLOK</th>
+          </tr>
+        </thead>
+          <tbody>
+            <?php foreach($daftarptn as $res): ?>
+            <tr>   
+                <td><?php echo $res->ptn_id; ?></td>
+                <td><?php echo $res->ptn_nama; ?></td>
+                <td><button type="button" class="btn btn-info">Lihat Profil</button></td>
+                <td><?php echo $res->wil_id; ?></td>
+                <td><?php echo $res->pl_nama; ?></td>
+            </tr>
+          <?php endforeach; ?>
+        </tbody>
+      </table>
+    </div>
   </div>
 </div>
