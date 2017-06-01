37eb45bd1b0fa5df8fc5b56d591705d2d8e2348d
mhanifamrizal <mhanifamrizal@gmail.com>
2017-06-01 23:32:55 +0700

    ad

---
+++ application/views/formcetak.php
@@ -0,0 +1,70 @@
+<div class="container" >
+  <div class="col-sm-12" style="background-color:lavender;">
+  <br>
+  <p class="text-center"><img src="http://news.unair.ac.id/wp-content/uploads/2017/04/SBMPTN-17.jpg" class="img-rounded" alt="Cinque Terre" width="1000" height="236"></p>
+  <h1><strong><center>Kartu Peserta SBMPTN</strong></center></h1>
+  <br><br/>
+  <h4><strong> NOMOR PESERTA : </strong></h4>
+  <h4><strong> NAMA DI IJAZAH : </strong></h4>
+  <h4><strong> NAMA DI LEMBAR JAWABAN (LJU) : </strong></h4>
+  <h4><strong> KELOMPOK UJIAN : </strong></h4>
+  <h4><strong> ALAMAT : </strong></h4>
+  <br>
+  <p class="container">
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>HARI & TANGGAL</th>
+        <th>WIB</th>
+        <th>KEGIATAN</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>S</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
+      </tr>
+      <tr>
+        <td>Mary</td>
+        <td>Moe</td>
+        <td>mary@example.com</td>
+      </tr>
+      <tr>
+        <td>July</td>
+        <td>Dooley</td>
+        <td>july@example.com</td>
+      </tr>
+    </tbody>
+  </table>
+</p>
+  <br><br/>
+  <h4><strong> PERLENGKAPAN YANG HARUS DIBAWA SAAT UJIAN : </strong></h4>
+  <p class="text-justify">- Kartu Tanda Peserta Ini</p>
+  <p class="text-justify">- Fotokopi Ijazah yang telah dilegalisasi atau tanda lulus asli</p>
+  <p class="text-justify">- Pensil 2B secukupnya,karet penghapus,peraut,pensil (jika diperlukan)</p>
+  <br>
+  <h4><strong> ALAMAT PADA SAAT MENGIKUTI UJIAN (HARUS DIISI) :</strong></h4>
+  <p class="text-justify">- ALAMAT :</p>
+  <p class="text-justify">- TELEPON :</p>
+  <br>
+  <h4><strong>PILIHAN PROGRAM STUDI :</strong></h4>
+  <p class="text-justify"><strong>1. UNIVERSITAS NEGERI SURABAYA</strong></p>
+  <p class="text-justify">502131 - P. SENI RUPA</p>
+  <p class="text-justify"><strong>2. UNIVERSITAS NEGERI SURABAYA</strong></p>
+  <p class="text-justify">502242 - P. JASMANI,KESEHATAN DAN REKREASI</p>
+  <p class="text-justify"><strong>3. UNIVERSITAS NEGERI MALANG</strong></p>
+    <p class="text-justify">502242 - P. JASMANI,KESEHATAN DAN REKREASI</p>
+    <br>
+    <h4><strong>LOKASI UJIAN :</strong></h4>
+    <p class="text-justify">- Sektor :</p>
+    <p class="text-justify">- Lokasi :</p>
+    <p class="text-justify">- Ruang :</p>
+    <br>
+    <h4><strong>PERNYATAAN :
+DENGAN INI SAYA MENYATAKAN BAHWA DATA YANG SAYA ISIKAN DALAM PENDAFTARAN ONLINE SBMPTN TAHUN 2017 ADALAH BENAR. SAYA BERSEDIA MENERIMA SANKSI PEMBATALAN PENERIMAAN DI PTN YANG SAYA PILIH APABILA MELANGGAR PERNYATAAN INI</strong></h4>
+<br>
+<h3><strong>TANDA TANGAN DAN NAMA TERANG : </strong></h3><br><br/>
+<br><br/>
+</div>
+</div>
