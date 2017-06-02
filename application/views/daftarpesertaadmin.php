a1d9fa80717544c64941ef0cc169c3b4ff1b5cf2
mhanifamrizal <mhanifamrizal@gmail.com>
2017-06-02 08:23:20 +0700

    qwerty

--- application/views/daftarpesertaadmin.php
+++ application/views/daftarpesertaadmin.php
@@ -23,10 +23,55 @@
                                         <td>08987654321</td>
                                         <td>yolo@gmail.com</td>
                                         <td>diterima/tidak</td>
-                                        <td><button type="button" class="btn btn-danger" onclick="myFunction()">Delete</button></td>
+                                        <td><a a data-toggle="modal" data-target="#tombolmodal"><button type="button" class="btn btn-primary" onclick="myFunction()">Tambah</button></a></td>
                                     </tr>
                                 </tbody>
                             </table>
                         </div>
                     </div>
                     </div>
+
+<div class="modal fade" id="tombolmodal" role="dialog">
+    <div class="modal-dialog">
+
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <div class="text-center">
+            <h2> Login Admin: </h2>
+        </div>
+        </div>
+
+        <div class="modal-body">
+              <div class="card">
+              <div class="card-block">
+
+                <div class="md-form">
+                    <i class="fa fa-envelope prefix"></i>
+                    <input type="text"  name="userid" class="form-control" value="Username" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Username';}" >
+                </div>
+
+                <div class="md-form">
+                    <i class="fa fa-lock prefix"></i>
+                    <input type="password"  name="pswrd" class="form-control" value="Password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Password';}">
+                </div>
+
+                <div class="text-center">
+                  <button class="btn btn-deep-purple" onclick="check(this.form)" type="submit"> Login</button>
+                </div>
+
+              </div>
+
+              <!--div class="modal-footer">
+                    <div class="options">
+                      <p>Lupa Password <a href="#">Password?</a></p>
+                    </div-->
+                </div>
+              </div>
+
+        </div>
+
+      </div>
+
+    </div>
